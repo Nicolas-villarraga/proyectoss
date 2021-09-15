fd0deef3a028240339cc0127fead18d7caff0d61
Nicolas-villarraga <nicovillarraga@hotmail.com>
2021-09-15 14:25:37 -0500

    integracion de templade
    landing pages

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('adminlte::page')
+
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h3 class="page__heading">Bienvenido</h3>
+        </div>
+        <div class="section-body">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h3 class="text-center"></h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
